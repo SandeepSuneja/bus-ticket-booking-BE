d0f6be9c868d4639cbc6bc5596242682285b9d32
Sandeep Suneja <sunny@Sandeeps-MacBook-Pro.local>
2024-04-15 01:15:18 +0530

    delete API added

--- bus-ticket-booking-BE/Controllers/BookingController.cs
+++ bus-ticket-booking-BE/Controllers/BookingController.cs
@@ -32,6 +32,19 @@ namespace bus_ticket_booking_BE.Controllers
             await _context.SaveChangesAsync();
             return Ok(booking.booking_id);
         }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteBooking(string id)
+        {
+            var booking = _context.Bookings.ToList().Find(p => p.booking_id == id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+            _context.Bookings.ToList().Remove(booking);
+            _context.SaveChanges();
+            return Ok();
+        }
     }
 }
 
